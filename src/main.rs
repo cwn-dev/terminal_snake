3d381cccbb9b707b64ade3694842f01a819907a9
craig <craig@nykindale.com>
2024-08-20 20:29:32 +0100

    Added the game loop & starting snake.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 extern crate libc;
 
-use libc::{termios, TCSANOW, tcgetattr, tcsetattr, STDIN_FILENO};
+use libc::{termios, TCSANOW, tcgetattr, tcsetattr, STDIN_FILENO, ioctl, winsize, TIOCGWINSZ};
 use std::io::{self, Read};
 use std::mem;
+use std::thread;
+use std::time::Duration;
 
 fn set_raw_mode() -> termios {
     let mut term = unsafe { mem::zeroed::<termios>() };
@@ -27,31 +29,63 @@ fn set_raw_mode() -> termios {
     original_term
 }
 
+fn get_console_size() -> (u16, u16) {
+    let mut ws = winsize {
+        ws_row: 0,
+        ws_col: 0,
+        ws_xpixel: 0,
+        ws_ypixel: 0,
+    };
+
+    unsafe {
+        ioctl(STDIN_FILENO, TIOCGWINSZ, &mut ws);
+    }
+
+    (ws.ws_col as u16, ws.ws_row as u16)
+}
+
 fn handle_input() {
     let mut buffer = [0; 3]; // Buffer to store input characters
 
-    while let Ok(_) = io::stdin().read(&mut buffer) {
-        if buffer[0] == 0x18 && buffer[1] == 0x00 && buffer[2] == 0x00 {
-            println!("CTRL+X");
-        }
+    let _ = io::stdin().read(&mut buffer);
+    if buffer[0] == 0x18 && buffer[1] == 0x00 && buffer[2] == 0x00 {
+        //println!("CTRL+X");
+    }
 
-        if buffer[0] == 0x1B && buffer[1] == 0x5B && buffer[2] == 0x44 {
-            println!("Left Arrow");
-        }
+    if buffer[0] == 0x1B && buffer[1] == 0x5B && buffer[2] == 0x44 {
+        //println!("Left Arrow");
+    }
 
-        if buffer[0] == 0x1B && buffer[1] == 0x5B && buffer[2] == 0x43 {
-            println!("Right Arrow");
-        }
+    if buffer[0] == 0x1B && buffer[1] == 0x5B && buffer[2] == 0x43 {
+        //println!("Right Arrow");
+    }
 
-        if buffer[0] == 0x1B && buffer[1] == 0x5B && buffer[2] == 0x41 {
-            println!("Up Arrow");
-        }
+    if buffer[0] == 0x1B && buffer[1] == 0x5B && buffer[2] == 0x41 {
+        //println!("Up Arrow");
+    }
 
-        if buffer[0] == 0x1B && buffer[1] == 0x5B && buffer[2] == 0x42 {
-            println!("Down Arrow");
-        }
-        
-        buffer = [0; 3]; // Clear buffer for next input
+    if buffer[0] == 0x1B && buffer[1] == 0x5B && buffer[2] == 0x42 {
+        //println!("Down Arrow");
+    }
+
+    buffer = [0; 3]; // Clear buffer for next input
+}
+
+fn draw_snake() {
+    let (cols, rows) = get_console_size();
+    let middle_x = (cols + 1) / 2;
+    let middle_y = (rows + 1) / 2;
+
+    print!("\x1b[{};{}f", middle_y, middle_x);
+    println!("║");
+}
+
+fn game_loop() {
+    loop {
+        handle_input();
+        draw_snake();
+
+        thread::sleep(Duration::from_secs(1/60));
     }
 }
 
@@ -66,9 +100,8 @@ fn main() {
     print!("\x1b[H");
 
     println!("Welcome to terminal_snake");
-
-    // Handle key presses
-    handle_input();
+    
+    game_loop();
 
     // Restore original terminal settings
     unsafe {
