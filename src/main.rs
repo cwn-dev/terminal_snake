a2c3d2059a40e4aa2d7572fc2e5a1805c6e68c45
craig <craig@nykindale.com>
2024-08-20 20:04:46 +0100

    Added input handling for arrow keys. Removed some old prototype functions.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate libc;
 
-use libc::{ioctl, termios, winsize, TIOCGWINSZ, TCSANOW, tcgetattr, tcsetattr, STDIN_FILENO};
+use libc::{termios, TCSANOW, tcgetattr, tcsetattr, STDIN_FILENO};
 use std::io::{self, Read};
 use std::mem;
 
@@ -27,39 +27,31 @@ fn set_raw_mode() -> termios {
     original_term
 }
 
-fn get_console_size() -> (u16, u16) {
-    let mut ws = winsize {
-        ws_row: 0,
-        ws_col: 0,
-        ws_xpixel: 0,
-        ws_ypixel: 0,
-    };
+fn handle_input() {
+    let mut buffer = [0; 3]; // Buffer to store input characters
 
-    unsafe {
-        ioctl(STDIN_FILENO, TIOCGWINSZ, &mut ws);
-    }
-
-    (ws.ws_col as u16, ws.ws_row as u16)
-}
-
-fn handle_key_presses() {
-    let mut buffer = [0; 1]; // Buffer to store input characters
-
-    while let Ok(_) = io::stdin().read_exact(&mut buffer) {
-        if buffer[0] == 0x18 {
+    while let Ok(_) = io::stdin().read(&mut buffer) {
+        if buffer[0] == 0x18 && buffer[1] == 0x00 && buffer[2] == 0x00 {
             println!("CTRL+X");
         }
+
+        if buffer[0] == 0x1B && buffer[1] == 0x5B && buffer[2] == 0x44 {
+            println!("Left Arrow");
+        }
+
+        if buffer[0] == 0x1B && buffer[1] == 0x5B && buffer[2] == 0x43 {
+            println!("Right Arrow");
+        }
+
+        if buffer[0] == 0x1B && buffer[1] == 0x5B && buffer[2] == 0x41 {
+            println!("Up Arrow");
+        }
+
+        if buffer[0] == 0x1B && buffer[1] == 0x5B && buffer[2] == 0x42 {
+            println!("Down Arrow");
+        }
         
-        buffer = [0; 1]; // Clear buffer for next input
-    }
-}
-
-fn print_line() {
-    print!("\x1b[2;1H");
-
-    for n in 3..5 {
-        println!("║");
-        print!("\x1b[{};1H", n);
+        buffer = [0; 3]; // Clear buffer for next input
     }
 }
 
@@ -73,14 +65,10 @@ fn main() {
     // Move to 0
     print!("\x1b[H");
 
-    // Get console size
-    let (width, height) = get_console_size();
-    println!("Console Width: {}, Console Height: {}", width, height);
-
-    print_line();
+    println!("Welcome to terminal_snake");
 
     // Handle key presses
-    handle_key_presses();
+    handle_input();
 
     // Restore original terminal settings
     unsafe {
