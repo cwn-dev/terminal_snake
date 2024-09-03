3c9a2773b4cb8dc334c5df76535af95d4ade4147
craig <craig@nykindale.com>
2024-09-03 22:02:25 +0100

    Food should no longer spawn on top of Snake.

--- src/state/food.rs
+++ src/state/food.rs
@@ -10,11 +10,16 @@ pub struct Food {
 impl Food {
     pub fn new_random(mut state: GameState, count: i8) -> Result<GameState, SnakeError> {
         let (cols, rows) = Arena::middle_coords(&state.arena)?;
-        let rand_cols = Random::time_seed().get(2, (cols - 3) as u128);
-        let rand_rows = Random::time_seed().get(4, (rows - 3) as u128);
+        let rand_cols = Random::time_seed().get(2, (cols - 3) as u128) as i16;
+        let rand_rows = Random::time_seed().get(4, (rows - 3) as u128) as i16;
 
         if (count as usize) > state.food.positions.len() {
             return Err(SnakeError);
+        }
+
+        // Tried to spawn food on top of part of Snake.
+        if state.snake.positions.iter().any(|&pos| (pos.x == rand_cols && pos.y == rand_rows)) {
+            return Food::new_random(state, count)
         }
 
         state.food.positions[0] = (rand_cols as i16, rand_rows as i16);
