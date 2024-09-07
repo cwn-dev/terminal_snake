012b8ddc250229d089f755cc4b680454b1fef1e4
craig <craig@nykindale.com>
2024-09-07 22:32:14 +0100

    Changed unicode enum to reflect the name of the char rather than what it will be.

--- src/engine/unicode_character.rs
+++ src/engine/unicode_character.rs
@@ -1,23 +1,23 @@
 pub enum UnicodeCharacter {
-    Food1,
-    Body1Horizontal,
-    Body1Vertical,
-    Body1TopLeftCorner,
-    Body1TopRightCorner,
-    Body1BottomLeftCorner,
-    Body1BottomRightCorner,
+    HeavyCircleWithCircleInside,
+    BoxDoubleHorizontal,
+    BoxDoubleVertical,
+    BoxDoubleDownAndRight,
+    BoxDoubleDownAndLeft,
+    BoxDoubleUpAnRight,
+    BoxDoubleUpAndLeft,
 }
 
 impl UnicodeCharacter {
     pub fn to_char(&self) -> char {
         match self {
-            UnicodeCharacter::Food1 => '\u{2B57}',                  // ⭗
-            UnicodeCharacter::Body1Horizontal => '\u{2550}',        // ═
-            UnicodeCharacter::Body1Vertical => '\u{2551}',          // ║
-            UnicodeCharacter::Body1TopLeftCorner => '\u{2554}',     // ╔
-            UnicodeCharacter::Body1TopRightCorner => '\u{2557}',    // ╗
-            UnicodeCharacter::Body1BottomLeftCorner => '\u{255A}',  // ╚
-            UnicodeCharacter::Body1BottomRightCorner => '\u{255D}', // ╝
+            UnicodeCharacter::HeavyCircleWithCircleInside => '\u{2B57}', // ⭗
+            UnicodeCharacter::BoxDoubleHorizontal => '\u{2550}',         // ═
+            UnicodeCharacter::BoxDoubleVertical => '\u{2551}',           // ║
+            UnicodeCharacter::BoxDoubleDownAndRight => '\u{2554}',       // ╔
+            UnicodeCharacter::BoxDoubleDownAndLeft => '\u{2557}',        // ╗
+            UnicodeCharacter::BoxDoubleUpAnRight => '\u{255A}',          // ╚
+            UnicodeCharacter::BoxDoubleUpAndLeft => '\u{255D}',          // ╝
         }
     }
 }
