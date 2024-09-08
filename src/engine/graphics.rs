7bf47773d0fc1b66d0e7676df40836446a24bd99
craig <craig@nykindale.com>
2024-09-08 19:54:35 +0100

    Added check to draw_char to make sure we don't draw at 0,0.

--- src/engine/graphics.rs
+++ src/engine/graphics.rs
@@ -7,9 +7,30 @@ impl Graphics {
         // todo: should I implement a check to ensure we don't
         // draw outside the available terminal area?
 
+        if y == 0 || x == 0 {
+            return Err(SnengineError::new(
+                format!("Cannot draw at {}, {}", x, y).as_str(),
+            ));
+        }
+
         print!("\x1b[{};{}f", y, x);
         print!("{}", char.to_char());
 
         Ok(())
     }
+}
+
+#[cfg(test)]
+mod tests {
+    // Todo: SnengineError should be an enum, or have some
+    // enum to go with it so the test below will work.
+
+    //use super::*;
+
+    // #[test]
+    // pub fn draw_char_error_if_x_0() {
+    //     let result = Graphics::draw_char(0, 42, Unicode::Space);
+    //     let expected = Err(SnengineError::ErrorType);
+    //     assert_eq!(result, expected);
+    // }
 }
